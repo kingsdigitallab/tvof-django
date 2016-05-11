c99309f8021a06a01cd0fe36f9ab0e049c91efa1
Neil Jakeman <neil.jakeman@kcl.ac.uk>
2016-05-11 15:03:22 +0100

    New basic page types

--- cms/models.py
+++ cms/models.py
@@ -1,11 +1,48 @@
 """Simple models for Wagtail."""
+
 # from __future__ import unicode_literals
 
-# from django.db import models
-
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
     """Basic home page."""
-    pass
+
+    subpage_types = ['IndexPage', 'RichTextPage', ]
+
+
+class IndexPage(Page):
+    """Streamfield richtextpage."""
+
+    subpage_types = ['IndexPage', 'RichTextPage', ]
+    content = StreamField([
+        ('heading', blocks.CharBlock(classname="")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('image_caption', blocks.CharBlock(classname="richtext-caption")),
+    ])
+
+IndexPage.content_panels = [
+    FieldPanel('title'),
+    StreamFieldPanel('content'),
+]
+
+
+class RichTextPage(Page):
+    """Streamfield richtextpage."""
+
+    content = StreamField([
+        ('heading', blocks.CharBlock(classname="")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('image_caption', blocks.CharBlock(classname="richtext-caption")),
+    ])
+
+RichTextPage.content_panels = [
+    FieldPanel('title'),
+    StreamFieldPanel('content'),
+]
