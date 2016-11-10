cfb1542c47772606a6992f7547650732e41f70e0
Miguel Vieira <jmvieira@gmail.com>
2016-11-10 12:40:05 +0000

    Changed urls to new Django format.

--- kiln/urls.py
+++ kiln/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('kiln.views',
-                       url(r'^json/(?P<kiln_url>[^?]*)$', 'process_to_json',
-                           {'template': 'process.html'}, name='kiln_to_json'),
-                       url(r'^(?P<kiln_url>[^?]*)$', 'process',
-                           {'template': 'process.html'}, name='kiln'),
-                       )
+from .views import process, process_to_json
+
+urlpatterns = [
+    url(r'^json/(?P<kiln_url>[^?]*)$', process_to_json,
+        {'template': 'process.html'}, name='kiln_to_json'),
+    url(r'^(?P<kiln_url>[^?]*)$', process,
+        {'template': 'process.html'}, name='kiln'),
+]
--- tvof/urls.py
+++ tvof/urls.py
@@ -1,43 +1,39 @@
 """This is the URLS."""
 from django.conf import settings
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
-# from django.core.urlresolvers import reverse
-# from django.views.generic.base import RedirectView
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
-# from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailsearch.signal_handlers import \
     register_signal_handlers as wagtailsearch_register_signal_handlers
 from wagtail.wagtailsearch.urls import frontend as wagtailsearch_frontend_urls
 
+admin.autodiscover()
 wagtailsearch_register_signal_handlers()
 
-admin.autodiscover()
-urlpatterns = patterns('',
-                       # url(r'^browse/', include(promrep_urls)),
-                       # url(r'^grappelli/', include('grappelli.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url('^{path}'.format(path=settings.KILN_CONTEXT_PATH),
-                           include('kiln.urls')),
-                       )
+urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url('^{path}'.format(path=settings.KILN_CONTEXT_PATH),
+        include('kiln.urls')),
+]
 
 try:
     if settings.DEBUG:
         import debug_toolbar
-        urlpatterns += patterns('',
-                                url(r'^__debug__/',
-                                    include(debug_toolbar.urls)),
-                                )
+        urlpatterns += [
+            url(r'^__debug__/',
+                include(debug_toolbar.urls)),
+        ]
 
 except ImportError:
     pass
 
-urlpatterns += patterns('',
-                        url(r'^search/', include(wagtailsearch_frontend_urls)),
-                        url(r'^wagtail/', include(wagtailadmin_urls)),
-                        url(r'', include(wagtail_urls)),
-                        )
+urlpatterns += [
+    url(r'^search/', include(wagtailsearch_frontend_urls)),
+    url(r'^wagtail/', include(wagtailadmin_urls)),
+    url(r'', include(wagtail_urls)),
+]
 
 if settings.DEBUG:
     from django.conf.urls.static import static
