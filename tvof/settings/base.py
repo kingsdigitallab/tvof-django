fbd26d408f808bb268902e3420022624ae66fd81
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2017-08-01 17:27:02 +0100

    Don't include google_analytics on dev and stg.

--- tvof/settings/base.py
+++ tvof/settings/base.py
@@ -198,7 +198,6 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'debug': False,
         },
     },
 ]
