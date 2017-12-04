485091ded1f42c02955fd8e811032143032823a9
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2017-12-04 19:41:44 +0000

    Fixed bug with creating new pattern that contians a -

--- text_patterns/urls.py
+++ text_patterns/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^api/(?P<slug>\w+)/(?P<root>patterns|move_pattern|segunits)/'
+    url(r'^api/(?P<slug>[\w-]+)/(?P<root>patterns|move_pattern|segunits)/'
         '(?P<path>.*?)/?$',
         views.patterns_api_view, name='patterns_api'),
-    url(r'^api/(?P<slug>\w+)/?$',
+    url(r'^api/(?P<slug>[\w-]+)/?$',
         views.patterns_api_view, name='patterns_api_root'),
-    url(r'^(?P<slug>\w+)/?$', views.pattern_set_view, name='pattern_set'),
+    url(r'^(?P<slug>[\w-]+)/?$', views.pattern_set_view, name='pattern_set'),
     url(r'$', views.pattern_sets_view, name='pattern_sets'),
 ]
