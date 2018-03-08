b6cefa90b4d2b8fe8b4348f91309a4cb474c46d4
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2018-03-08 10:35:07 +0000

    WIP: print version of the text.

--- text_viewer/urls.py
+++ text_viewer/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import view_text_viewer, view_text_viewer_api
+from .views import view_text_viewer, view_text_viewer_api, view_text_print
 
 urlpatterns = [
+    url(r'^print/(.*)$', view_text_print, name='text_print'),
     url(r'^api/(.*)$', view_text_viewer_api, name='text_viewer_api'),
     url(r'^(.*)$', view_text_viewer, name='text_viewer'),
 ]
