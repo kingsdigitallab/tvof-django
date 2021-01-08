5f9a9eaeeb078af7a19a4c0d624982dd7e2e71ec
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2021-01-08 01:06:35 +0000

    AC-410.2: removed the unlemmatised tokens such as roman numbers from the search index (e.g. c).

--- tvof/text_search/es_indexes.py
+++ tvof/text_search/es_indexes.py
@@ -131,7 +131,6 @@ class AnnotatedToken(Document):
             elif k == 'preceding':
                 field = k
                 ret.previous_word = v.split(' ')[-1]
-            # TODO: verse_cat, speech_cat
 
             if field is None and hasattr(ret, k):
                 field = k
@@ -145,6 +144,10 @@ class AnnotatedToken(Document):
         ret.meta.id = '{}.{:03d}'.format(attrib['location'], int(attrib['n']))
 
         ret.set_derived_fields()
+
+        if not str(getattr(ret, 'lemma', '') or '').strip():
+            # we don't index unlemmatised tokens (asked by partners, 01/2021)
+            return []
 
         return [ret]
 
@@ -372,7 +375,7 @@ class Indexer:
             print('WARNING: {} indexed != {} expected'.format(count, total))
 
     def _get_selected_indexes(self, index_names=None):
-        '''Returns a list of index inforamtion.
+        '''Returns a list of index information.
         One item per name in index_names.
         Thrown Exception if name not found.'''
         ret = [
