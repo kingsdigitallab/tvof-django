ef2de15be884de2a2ad90a46a1f0a4edfdb84a98
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2024-02-09 18:03:44 +0000

    WIP: simplified and aligned docker compose & dockerfile to other gitlab projects. Now build completing.

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ def makedir(apath):
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+DEBUG = not env.bool("PRODUCTION", False)
 # Local time zone. Choices are
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["tvof.ac.uk"])
+ALLOWED_HOSTS = env.list("VIRTUAL_HOST", default=["tvof.ac.uk"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
